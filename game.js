44ad7196e4d53cfe7ee4ff5bc7b22c7d8a01b0bd
lawbrewing <lawmj04@gmail.com>
2026-01-23 12:47:50 -0600

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let CONFIG = {
 
 let SPRITE_DATA = {
     customer: { h: 369, frames: 1 }, // We'll detect frames automatically
-    tower: { h: 364, cols: 3 },
+    tower: { h: 433, cols: 3 },
     taps: [
         { h: 150, offsetX: -1, offsetY: -15 }, 
         { h: 150, offsetX: -32, offsetY: -9 }, 
