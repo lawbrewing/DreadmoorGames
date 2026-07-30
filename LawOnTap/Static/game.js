2819ed23aefc6d9f2bcb61bdfde755ce7688299a
lawbrewing <lawmj04@gmail.com>
2026-07-29 19:46:05 -0500

    Adjust tap positions and clipping for neighbors

--- LawOnTap/Static/game.js
+++ LawOnTap/Static/game.js
@@ -67,17 +67,18 @@ let SPRITE_DATA = {
     ],
 },
     // 3 INDEPENDENT TAPS
+    // 3 INDEPENDENT TAPS
     taps_visual: { 
         s: .5, 
         positions: [
-            // Left Tap: 90 deg clockwise (Shaved 30px off the TOP of the open sprite)
-            { x: 377,  y: 85,  openRotation: Math.PI / 2,   openOffset: { x: 241, y: 171 }, openClip: { sy: 10, trimH: 10 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }, 
+            // Left Tap: 90 deg clockwise (Pushed down 40px to clear the top neighbor)
+            { x: 377,  y: 85,  openRotation: Math.PI / 2,   openOffset: { x: 241, y: 171 }, openClip: { sy: 40, trimH: 40 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }, 
             
-            // Middle Tap: 180 deg (Shaved 25px off the TOP of the open sprite)
-            { x: 968,  y: 89,  openRotation: Math.PI,       openOffset: { x: 0, y: 470 },   openClip: { sy: 10, trimH: 10 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }, 
+            // Middle Tap: 180 deg (Pushed down 35px to clear the top neighbor)
+            { x: 968,  y: 89,  openRotation: Math.PI,       openOffset: { x: 0, y: 470 },   openClip: { sy: 35, trimH: 35 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }, 
             
-            // Right Tap: 90 deg counter-clockwise (Shaved 25px off the TOP of the open sprite)
-            { x: 1576, y: 83,  openRotation: -Math.PI / 2,  openOffset: { x: -245, y: 182 }, openClip: { sy: 10, trimH: 10 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }  
+            // Right Tap: 90 deg counter-clockwise (Pushed down 35px to clear the top neighbor)
+            { x: 1576, y: 83,  openRotation: -Math.PI / 2,  openOffset: { x: -245, y: 182 }, openClip: { sy: 35, trimH: 35 }, clip: { sx: 0, sy: 0, trimW: 0, trimH: 0 } }  
         ],
     },
     
